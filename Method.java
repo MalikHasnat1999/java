579d5e66a6411b5cc1d66e8d022155d1b55fcc92
Malik Hasnat <hasnathasnat48@gmail.com>
2020-10-13 17:06:16 +0500

    methods and their return values

---
+++ Method.java
@@ -0,0 +1,19 @@
+
+
+public class Method{
+
+	static int add(){
+	int a = 1;
+	int b = 2;
+
+	int c = a + b;
+
+	return c;
+}
+
+	public static void main(String[] args) {
+		
+		System.out.println("a + b = "+add());
+	}
+}
+
