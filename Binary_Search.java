47b6280d496741cdec1621be0ff2a3f3473c8b6e
Malik Hasnat <hasnathasnat48@gmail.com>
2020-10-24 15:57:33 +0500

    Binary Search

--- Binary_Search.java
+++ Binary_Search.java
@@ -1,11 +1,11 @@
 public class Binary_Search{
 	public static void main(String[] args) {
 		
-		int[] arr = {1,2,3,4,5,6,7,8,9,10};
+		int[] arr = {10,20,30,40,50,60,70,80,90,100};
 
 		int start = 0;
 		int end  = 9;
-		int item = 5;
+		int item = 90;
 		int mid;
 		int c = -1; 
 
